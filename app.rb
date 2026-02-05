163071cdf0469fd343d7cafb06cf10af8cdfbbc3
Philip27312731 <philip.bernin@elev.ga.ntig.se>
2026-02-05 16:29:42 +0100

    vecka 6
    ewratrytuyt

--- app.rb
+++ app.rb
@@ -3,3 +3,37 @@ require 'slim'
 require 'sqlite3'
 require 'sinatra/reloader'
 require 'bcrypt'
+
+enable :sessions
+
+get '/' do
+	slim :index
+end
+
+get '/play' do
+	redirect '/login' unless session[:user]
+	slim :play
+end
+
+get '/login' do
+	slim :login
+end
+
+post '/login' do
+	session[:user] = params['email'] || params['username'] || 'player'
+	redirect '/play'
+end
+
+get '/register' do
+	slim :register
+end
+
+post '/register' do
+	session[:user] = params['email'] || params['username'] || 'player'
+	redirect '/play'
+end
+
+get '/logout' do
+	session.clear
+	redirect '/'
+end
